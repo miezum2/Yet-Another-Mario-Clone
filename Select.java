ecf41cf1257da3c4b2f1d0ccdb581447a02a5840
Nobody150 <33348118+Nobody150@users.noreply.github.com>
2017-12-09 10:21:50 +0100

    Select
    Button für Editor

---
+++ Select.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Select here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Select extends Actor
+{
+    private String name;
+    
+    public Select (String name, String image)
+    {
+        this.name = name;
+        this.setImage(image);
+    }
+    
+    
+}
