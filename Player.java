0a8196b6253bef42f6a67d73f1a6cb073680ff12
Nobody150 <33348118+Nobody150@users.noreply.github.com>
2017-12-12 21:44:03 +0100

    Controls

--- Player.java
+++ Player.java
@@ -8,7 +8,12 @@ import java.util.*;
 public class Player extends Entity
 {
     private Movement movement;
-    private String[] controls;    
+    private String[] controls;  
+    private boolean leftDown = false;
+    private boolean rightDown = false;
+    private boolean jumpenable = true;
+    private boolean jumpabel = true;
+    private boolean directionChange=false;
     /**
      * 
      */
@@ -19,19 +24,22 @@ public class Player extends Entity
         /* Konstruktor der Superklasse aufrufen*/
     }
     
-    public Player(String name, String id, double x, double y, GreenfootImage image, String state)
+    public Player(String name, String id, double x, double y, GreenfootImage image, String state, String[] controls)
     {
         super(name, id, x, y, image, state);
         movement = new Movement(0, 0.1);
         setOrientation("right");
         setActivity("standing");
+        this.controls=controls;
     }
     
+    /*
     public Player(Entity entity)
     {
         this(entity.getName(), entity.getId(), entity.getPosX(), entity.getPosY(), entity.getImage(), entity.getState());
     }
-    
+    */
+   
     public Player()
     {
         
@@ -50,12 +58,7 @@ public class Player extends Entity
         */
     }
     
-    private boolean leftDown = false;
-    private boolean rightDown = false;
-    //private int jumpCount = 0;
-    private boolean jumpanable = true;
-    private boolean jumpabel = true;
-    private boolean directionChange=false;
+    
     
     public void update(List<Entity> entities)
     {
@@ -82,146 +85,154 @@ public class Player extends Entity
         }    
     }
     
+    /**
+     * Methode simulate
+     *
+     * @param entities Ein Parameter
+     */
     public void simulate(List<Entity> entities)
     {
         // Aktuelle Welt an Movement übergeben, um Kollisionsprüfung zu erlauben
         movement.setEntities(entities);         
+        //jumpabel = true fals er den Boden berührt
         jumpabel = movement.isTouchingObjectBelow(getPosX(), getPosY(), getWidthUnits(), getHeightUnits(), Block.class);
         
-        // Mario steuer
-        if (getName().equals("Mario"))
+        //Bewegung nach links
+        if(Greenfoot.isKeyDown(controls[1]))
         {
-            if(Greenfoot.isKeyDown("w"))
+            if (!rightDown)
             {
-                if (jumpabel)
+                setPosX(movement.move(180, getPosX(), getPosY(), getWidthUnits(), getHeightUnits()));
+                leftDown = true;
+                setOrientation("left");
+                if ((getActivity()!="jumping"))
                 {
-                    if (jumpanable)
-                    {
-                        /*
-                        if (jumpCount==5)
-                        {
-                           setPosY(getPosY() + movement.jump(3)); 
-                           jumpCount=0;
-                        }
-                        else
-                        */
-                        {
-                            setPosY(getPosY() + movement.jump(1));
-                            setActivity("jumping");
-                        }
-                        jumpanable=false;
-                        jumpabel = false;
-                    }
-                    else
-                    {
-                        if (movement.getYMove() <= 0)
-                        {
-                            jumpanable = true; 
-                        }
-                    }
+                    setActivity("walking");
                 }
             }
-            if(Greenfoot.isKeyDown("a"))
+            if (movement.getSpeed()>0)
             {
-                if (!rightDown)
-                {
-                    setPosX(movement.move(180, getPosX(), getPosY(), getWidthUnits(), getHeightUnits()));
-                    leftDown = true;
-                    setOrientation("left");
-                    if ((getActivity()!="jumping"))
-                    {
-                        setActivity("walking");
-                    }
-                }
-                if (movement.getSpeed()>0)
-                {
-                    directionChange=true;
-                }
+                directionChange=true;
             }
-            else
-            {
-                if (movement.isTouchingLeftObject(getPosX(), getPosY(), getWidthUnits(), getHeightUnits(), Block.class))
-                    {
-                        movement.setSpeed(0);
-                        leftDown = false;
-                    }
-                    else
-                    {
-                        if (leftDown)
-                        {
-                            if (movement.getSpeed()>0)
-                            {
-                                directionChange=true;
-                            }
-                            if (movement.getSpeed() < 0 )
-                            {
-                                setPosX(movement.move(0, getPosX(), getPosY(), getWidthUnits(), getHeightUnits()));
-                                if ((movement.getSpeed()<1) && (movement.getSpeed()<0) && directionChange)
-                                {
-                                    setActivity("braking");
-                                    setOrientation("right");
-                                }
-                            }
-                            else
-                            {
-                                movement.setSpeed(0);
-                                directionChange=false;
-                                leftDown = false;
-                            }
-                        }
-                    }
-            }
-            if(Greenfoot.isKeyDown("s"))
-            {
-                
-            }
-            if(Greenfoot.isKeyDown("d"))
-            {
-                if (!leftDown)
-                {
-                    setPosX(movement.move(0, getPosX(), getPosY(), getWidthUnits(), getHeightUnits()));
-                    rightDown = true;
-                    setOrientation("right");
-                    if ((getActivity()!="jumping"))
-                    {
-                        setActivity("walking");
-                    }
-                }
-                if (movement.getSpeed()<0)
-                {
-                    directionChange=true;
-                }
-            }
-            else
-            {
-                if (movement.isTouchingRightObject(getPosX(), getPosY(), getWidthUnits(), getHeightUnits(), Block.class))
+        }
+        else
+        {
+            if (movement.isTouchingLeftObject(getPosX(), getPosY(), getWidthUnits(), getHeightUnits(), Block.class))
                 {
                     movement.setSpeed(0);
-                    rightDown = false;
+                    leftDown = false;
                 }
                 else
                 {
-                    if (rightDown)
+                    if (leftDown)
                     {
-                        if (movement.getSpeed() > 0 )
+                        if (movement.getSpeed()>0)
                         {
-                            setPosX(movement.move(180, getPosX(), getPosY(), getWidthUnits(), getHeightUnits()));
-                            if ((movement.getSpeed()<1) && (movement.getSpeed()>0) && directionChange)
+                            directionChange=true;
+                        }
+                        if (movement.getSpeed() < 0 )
+                        {
+                            setPosX(movement.move(0, getPosX(), getPosY(), getWidthUnits(), getHeightUnits()));
+                            if ((movement.getSpeed()<1) && (movement.getSpeed()<0) && directionChange)
                             {
                                 setActivity("braking");
-                                setOrientation("left");
+                                setOrientation("right");
                             }
                         }
                         else
                         {
                             movement.setSpeed(0);
-                            rightDown = false;
                             directionChange=false;
+                            leftDown = false;
                         }
                     }
                 }
+        }
+        if(Greenfoot.isKeyDown(controls[2]))
+        {
+            
+        }
+        //Bewegung nach rechts
+        if(Greenfoot.isKeyDown(controls[3]))
+        {
+            if (!leftDown)
+            {
+                setPosX(movement.move(0, getPosX(), getPosY(), getWidthUnits(), getHeightUnits()));
+                rightDown = true;
+                setOrientation("right");
+                if ((getActivity()!="jumping"))
+                {
+                    setActivity("walking");
+                }
             }
-        }    
+            if (movement.getSpeed()<0)
+            {
+                directionChange=true;
+            }
+        }
+        else
+        {
+            if (movement.isTouchingRightObject(getPosX(), getPosY(), getWidthUnits(), getHeightUnits(), Block.class))
+            {
+                movement.setSpeed(0);
+                rightDown = false;
+            }
+            else
+            {
+                if (rightDown)
+                {
+                    if (movement.getSpeed() > 0 )
+                    {
+                        setPosX(movement.move(180, getPosX(), getPosY(), getWidthUnits(), getHeightUnits()));
+                        if ((movement.getSpeed()<1) && (movement.getSpeed()>0) && directionChange)
+                        {
+                            setActivity("braking");
+                            setOrientation("left");
+                        }
+                    }
+                    else
+                    {
+                        movement.setSpeed(0);
+                        rightDown = false;
+                        directionChange=false;
+                    }
+                }
+            }
+        }
+        //Sprung
+        if(Greenfoot.isKeyDown(controls[4]))
+        {
+            //prüft ob Spiele Springen darf, also am Boden angekommen ist
+            if (jumpabel)
+            {
+                //prüft ob 
+                if (jumpenable)
+                {
+                    /*
+                    if (jumpCount==5)
+                    {
+                       setPosY(getPosY() + movement.jump(3)); 
+                       jumpCount=0;
+                    }
+                    else
+                    */
+                    {
+                        setPosY(getPosY() + movement.jump(1));
+                        setActivity("jumping");
+                    }
+                    jumpenable=false;
+                    jumpabel = false;
+                }
+                else
+                {
+                    if (movement.getYMove() <= 0)
+                    {
+                        jumpenable = true; 
+                    }
+                }
+            }
+        }
+        
         if (movement.getYMove()==0 && movement.getSpeed()==0)
         {
             setActivity("standing");
